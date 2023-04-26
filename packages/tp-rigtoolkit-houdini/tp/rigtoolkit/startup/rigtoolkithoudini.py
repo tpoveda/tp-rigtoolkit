27471e809654329df2ad1f43383f3b1ac79ff753
Tomi Poveda <tpovedatd@gmail.com>
2023-04-26 03:46:35 +0200

    Added tp-rigtoolkit-houdini package

--- packages/tp-rigtoolkit-houdini/tp/rigtoolkit/startup/rigtoolkithoudini.py
+++ packages/tp-rigtoolkit-houdini/tp/rigtoolkit/startup/rigtoolkithoudini.py
@@ -1,15 +1,9 @@
 import os
-import sys
 import inspect
 
-from Qt.QtWidgets import QApplication
-
 from tp.core import log
-from tp.core.managers import resources
 from tp.bootstrap.core import manager, exceptions as bootstrap_exceptions
 from tp.common.python import path
-
-from tp.rigtoolkit.maya.plugins import loader
 
 logger = log.tpLogger
 
@@ -27,15 +21,7 @@ def startup(package_manager):
 	if not package:
 		raise bootstrap_exceptions.MissingPackage(package)
 
-	logger.info('Loadding tp-rigtoolkit-maya package...')
-
-	# Make sure QApplication instance exists before registering resources
-	app = QApplication.instance() or QApplication(sys.argv)
-	resources_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'maya', 'resources')
-	resources.register_resource(resources_path, key='tp-rigtoolkit-maya')
-
-	logger.info('Loading tpRigToolkit Maya plugins...')
-	loader.load_all_rigtoolkit_plugins()
+	logger.info('Loadding tp-rigtoolkit-houdini package...')
 
 
 def shutdown(package_manager):
@@ -43,8 +29,4 @@ def shutdown(package_manager):
 	Shutdown function that is called during tpDcc framework shutdown.
 	This function is called at the end of tpDcc framework shutdown.
 	"""
-
-	logger.info('Shutting down tp-rigtoolkit-maya package...')
-
-	logger.info('Unloading tpRigToolkit Maya plugins...')
-	loader.unload_all_rigtoolkit_plugins()
+	logger.info('Shutting down tp-rigtoolkit-houdini package...')
